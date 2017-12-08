d85fc9b22aa76a8bffb62cd9cad026df4f11087c
polyfloyd <floyd@polyfloyd.net>
2017-12-08 21:10:25 +0100

    Rewrite Board to ES2015

--- src/js/game.js
+++ src/js/game.js
@@ -1,101 +1,89 @@
-'use strict';
+let _idEnum = 0;
 
-var Board = function(grid) {
-	this.grid = grid || [
-		[{val: 0}, {val: 0}, {val: 0}, {val: 0}],
-		[{val: 0}, {val: 0}, {val: 0}, {val: 0}],
-		[{val: 0}, {val: 0}, {val: 0}, {val: 0}],
-		[{val: 0}, {val: 0}, {val: 0}, {val: 0}],
-	];
-};
-
-Board._idEnum = 0;
-
-Board.prototype.flat = function(value) {
-	return this.grid.reduce(function(result, row, y) {
-		return row.reduce(function(result, cell, x) {
-			result.push({
-				x:      x,
-				y:      y,
-				val:    cell.val,
-				id:     cell.id,
-				merged: cell.merged,
+class Board {
+	constructor(grid) {
+		if (!grid) {
+			this.grid = Array.from({ length: 4 }, () => {
+				return Array.from({ length: 4 }, () => ({val: 0}));
 			});
-			return result;
-		}, result);
-	}, []);
-};
-
-Board.prototype.count = function(value) {
-	return this.flat().filter(function(cell) {
-		return cell.val == value;
-	});
-};
-
-Board.prototype.equals = function(other) {
-	return this.grid.every(function(row, y) {
-		return row.every(function(cell, x) {
-			return other.grid[y][x].val == cell.val;
-		});
-	});
-}
-
-Board.prototype.clone = function() {
-	var grid = this.grid.map(function(row) {
-		return row.map(function(cell) {
-			return cell;
-		});
-	});
-	return new Board(grid);
-};
-
-Board.prototype.rotate = function(deg) {
-	switch ((deg + 360) % 360) {
-	case 0:
-		return this.clone();
-	case 90:
-		return new Board([
-			[this.grid[0][3], this.grid[1][3], this.grid[2][3], this.grid[3][3]],
-			[this.grid[0][2], this.grid[1][2], this.grid[2][2], this.grid[3][2]],
-			[this.grid[0][1], this.grid[1][1], this.grid[2][1], this.grid[3][1]],
-			[this.grid[0][0], this.grid[1][0], this.grid[2][0], this.grid[3][0]],
-		]);
-	case 180:
-		return new Board(this.grid.map(function(row) {
-			return row.reverse();
-		}).reverse());
-	case 270:
-		return this.rotate(90).rotate(180);
-	default:
-		throw new Error('Can not rotate board by '+deg+'deg');
-	}
-};
-
-// Shifts the cells in the field to the left.
-Board.prototype.shift = function() {
-	return new Board(this.grid.map(function(row) {
-		var newRow = row.filter(function(cell) {
-			return cell.val != 0;
-		}).reduce(function(tuple, cell, x) {
-			var newRow = tuple[0];
-			if (!newRow.length) {
-				return [[cell], false];
-			}
-			if (newRow[newRow.length-1].val == cell.val && !tuple[1]) {
-				return [newRow.slice(0, newRow.length-1).concat([{
-					val:    cell.val * 2,
-					id:     ++Board._idEnum,
-					merged: [newRow[newRow.length-1].id, cell.id],
-				}]), true];
-			}
-			return [newRow.concat([cell]), false];
-		}, [[], false])[0];
-		for (var i = newRow.length; i < 4; i++) {
-			newRow.push({val: 0});
+		} else {
+			this.grid = grid;
 		}
-		return newRow;
-	}));
-};
+	}
+
+	flat() {
+		return this.grid.reduce((result, row, y) => {
+			return row.reduce((result, cell, x) => {
+				return result.concat([{
+					x,
+					y,
+					val:    cell.val,
+					id:     cell.id,
+					merged: cell.merged,
+				}]);
+			}, result);
+		}, []);
+	}
+
+	count(value) {
+		return this.flat()
+			.filter(cell => cell.val == value);
+	}
+
+	equals(other) {
+		return this.grid.every((row, y) => {
+			return row.every((cell, x) => {
+				return other.grid[y][x].val == cell.val;
+			});
+		});
+	}
+
+	clone() {
+		return new Board(this.grid.map(row => row.map(cell => cell)));
+	}
+
+	rotate(deg) {
+		switch ((deg + 360) % 360) {
+			case 0:
+				return this.clone();
+			case 90:
+				return new Board([
+					[this.grid[0][3], this.grid[1][3], this.grid[2][3], this.grid[3][3]],
+					[this.grid[0][2], this.grid[1][2], this.grid[2][2], this.grid[3][2]],
+					[this.grid[0][1], this.grid[1][1], this.grid[2][1], this.grid[3][1]],
+					[this.grid[0][0], this.grid[1][0], this.grid[2][0], this.grid[3][0]],
+				]);
+			case 180:
+				return new Board(this.grid.map((row) => row.reverse()).reverse());
+			case 270:
+				return this.rotate(90).rotate(180);
+			default:
+				throw new Error(`Can not rotate board by ${deg}deg`);
+		}
+	}
+
+	// Shifts the cells in the field to the left.
+	shift() {
+		return new Board(this.grid.map(row => {
+			const [ newRow, ] = row
+				.filter(cell => cell.val !== 0)
+				.reduce(([newRow, prevMerged], cell, x) => {
+					if (!newRow.length) {
+						return [[cell], false];
+					}
+					if (newRow[newRow.length-1].val == cell.val && !prevMerged) {
+						return [newRow.slice(0, newRow.length-1).concat([{
+							val:    cell.val * 2,
+							id:     ++_idEnum,
+							merged: [newRow[newRow.length-1].id, cell.id],
+						}]), true];
+					}
+					return [newRow.concat([cell]), false];
+				}, [[], false]);
+			return newRow.concat(Array.from({ length: 4 - newRow.length }, () => ({val: 0})));
+		}));
+	}
+}
 
 
 export var Game = function() {
@@ -106,7 +94,7 @@ export var Game = function() {
 	for (var i = 0; i < 2; i++) {
 		var empty = this.board.count(0);
 		var cell = empty[(Math.random() * empty.length) | 0];
-		this.board.grid[cell.y][cell.x] = {val: 2, id: ++Board._idEnum};
+		this.board.grid[cell.y][cell.x] = {val: 2, id: ++_idEnum};
 	}
 };
 
@@ -139,7 +127,7 @@ Game.prototype.move = function(dir) {
 		return;
 	}
 
-	var newId = ++Board._idEnum;
+	var newId = ++_idEnum;
 	var empty = this.board.count(0);
 	var cell = empty[(Math.random() * empty.length) | 0];
 	this.board.grid[cell.y][cell.x] = {val: [2, 4][Math.round(Math.random())], id: newId};
@@ -161,7 +149,7 @@ Game.prototype.move = function(dir) {
 		},
 	});
 
-	if (this.board.count(0).length == 0) {
+	if (this.board.count(0).length === 0) {
 		var movePossible = [0, 90, 180, 270].some(function(deg) {
 			return this.board.rotate(deg).shift().count(0).length > 0;
 		}.bind(this));
@@ -182,7 +170,9 @@ Game.prototype.move = function(dir) {
 
 Game.prototype.score = function() {
 	return this.board.flat().reduce(function(sum, cell) {
-		if (cell.val <= 2) return sum;
-		return sum + cell.val * Math.log2(cell.val>>1);
+		if (cell.val <= 2) {
+			return sum;
+		}
+		return sum + cell.val * Math.log2(cell.val >> 1);
 	}, 0);
 };
